1fc6739bbfe6b08c2ae350cd4f4b5cb43228748b
Skamer-li <plazerovich@gmail.com>
2024-12-20 10:27:04 +0200

    changed paths to scenes in scripts

--- scripts/settings_menu.gd
+++ scripts/settings_menu.gd
@@ -38,8 +38,7 @@ func center_window():
 	get_window().set_position(screen_center - window_size / 2)
 
 func _on_back_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://menu.tscn")
-
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 func _on_fullscreen_button_pressed() -> void:
 	if (DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_FULLSCREEN):
