3c53d3aba820f29c1232dc44dfa3bc6db05b9ea6
Ivan Bomko <ivan.bomko@epita.fr>
2024-12-20 13:44:25 +0100

    protorype of captain stats and applying his class to main player script

---
+++ scripts/CaptainStats.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name CaptainStats
+
+func _init(): # unique stats 
+	var base_strenghts: int = 5
+	var food_required: int = base_strenghts
+	var health_points: int = base_strenghts
+	var character_name: String = "Captain"
+	
+	
+	
