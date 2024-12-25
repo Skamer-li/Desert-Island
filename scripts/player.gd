2bbfc190730f7d3d95b15f9065e34ab214a01fee
Ivan Bomko <ivan.bomko@epita.fr>
2024-12-25 19:06:40 +0100

    Added food, fatecoins, daycounter containers icons. Started writing connections to func ready to update stats

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,5 @@
 extends Node2D  
-#var player = $Player
-#var captain = CaptainStats.new()
-#player.set_character(captain)
-# character stats variables
+
 var food_coins: Array = []  # amount of food coins in array, 0 - 1p and 1 - 4p
 var food_points: int = food_coins[0] # how many points of food you have
 var health_points: int = 1  # HP based on character card (depends on char card)
@@ -10,16 +7,26 @@ var fate_coins: int = 1  # amount of fate coins (skulls)
 var ally_card: String = "None"  # random ally character
 var enemy_card: String = "None" # random enemy card
 var items: Array = []  # first idea of inventory (items)
-
-# Constants
 var food_points_loop: int = 2  # amount of food consumption each loop
 var hp_loss: int = 1  # hunger penalty
 
 # Function to initialize player stats
 func _ready():
-	#player.modify_food_coins(4, 0)
-	#player.modify_food_points(food_coins[0] * 1 + food_coins[1] * 4)
-	#player.modify_ally_card("Hero_name")
-	#player.modify_enemy_card("Hero_name")
 
 	emit_signal("stats_updated")
+
+@onready var day_counter_label = $DayCounterLabel
+@onready var foodquantity1_label = $FoodLabel
+func _loop():
+	$HPValueLabel.text = health_points
+	$FateCoinLabel.text = fate_coins
+	update_food()
+	update_day(1)
+	
+func update_day(day):
+	day_counter_label.text = "Day: " + str(day)
+	
+func update_food():
+	foodquantity1_label.text = food_points - food_points_loop
+	#embrion of food consumtion update
+	
