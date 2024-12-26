1b7c4ee1a0a0e7d09f0771ef2c2f76e86fef3ac5
Ivan Bomko <ivan.bomko@epita.fr>
2024-12-26 21:44:06 +0100

    func and char data changes

---
+++ scripts/CharacterData.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+class_name CharacterData_Captain
+
+	@export var base_strenghts: int = 5
+	@export var food_required: int = base_strenghts
+	@export var health_points: int = base_strenghts
+	@export var character_name: String = "Captain"
+	@export var enemy_card: String = "Hero name"
