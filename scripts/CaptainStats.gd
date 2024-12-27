399a502178b8433361ce357923d7e50471168cf5
Skamer-li <plazerovich@gmail.com>
2024-12-27 22:03:28 +0200

    sigma

---
+++ scripts/CaptainStats.gd
@@ -0,0 +1,13 @@
+extends Resource
+
+class_name CaptainStats
+
+func _init(): # unique stats 
+	var base_strenghts: int = 5
+	var food_required: int = base_strenghts
+	var health_points: int = base_strenghts
+	var character_name: String = "Captain"
+	var enemy_card: String = "Hero name"
+	
+	
+	
