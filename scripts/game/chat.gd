d3dc2ed2668f69e60dfaaa1d6c212dc42f8d520b
UglRar <rostislavramensky@gmail.com>
2025-07-24 02:48:51 +0300

    chat completed

--- scripts/game/chat.gd
+++ scripts/game/chat.gd
@@ -1,11 +1,24 @@
 extends Control
+var player_id=1
+var char
+var initialized = false
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@rpc("any_peer","call_local")		
+func init(id) -> void:
+	player_id=id
+	for player in get_parent().get_node("players").get_children():
+		if player.player_id==player_id:
+			char=get_node(get_path_to(player))
+			initialized=true
+			
+func _on_line_edit_text_submitted(new_text: String) -> void:
+	if initialized==false:
+		for player in get_parent().get_node("players").get_children():
+			init.rpc_id(player.player_id,player.player_id)
+	
+	if new_text.replace(" ","")=="":
+		pass
+	else: 
+		GameManager.send_message.rpc(new_text,char.player_name,char.character_name)
+		$Panel/VBoxContainer/LineEdit.clear()
